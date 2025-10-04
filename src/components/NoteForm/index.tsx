89b0f1c1a7b97513021c01c14566c9e17ef06db4
Felipe Almeida <felipeortalmeida@gmail.com>
2025-10-04 12:34:27 -0300

    feat: setup basic data binding

--- src/components/NoteForm/index.tsx
+++ src/components/NoteForm/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
+
 const NoteForm = () => {
+    const [title, setTitle] = useState("")
+    const [priority, setPriority] = useState("")
+    const [category, setCategory] = useState("")
+    const [description, setDescription] = useState("")
+
+    console.log(title, priority, category, description)
+
     return <form className="flex flex-col items-stretch justify-center gap-3">
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="title">Title</label>
-            <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="title" name="title" placeholder="fix github actions yml" />
+            <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="title" name="title" placeholder="fix github actions yml" value={title} onChange={(e) => { setTitle(e.target.value) }} />
         </div>
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="priority">Priority</label>
-            <select className="px-2 py-1 border border-neutral-300 rounded-xl" name="priority" id="priority">
+            <select className="px-2 py-1 border border-neutral-300 rounded-xl" name="priority" id="priority" value={priority} onChange={(e) => { setPriority(e.target.value) }}>
                 <option value="">select a priority</option>
                 <option>high</option>
                 <option>medium</option>
@@ -15,7 +24,7 @@ const NoteForm = () => {
         </div>
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="category">Category</label>
-            <select className="px-2 py-1 border border-neutral-300 rounded-xl" name="category" id="category">
+            <select className="px-2 py-1 border border-neutral-300 rounded-xl" name="category" id="category" value={category} onChange={(e) => { setCategory(e.target.value) }}>
                 <option value="">select a category</option>
                 <option>work</option>
                 <option>personal</option>
@@ -23,7 +32,7 @@ const NoteForm = () => {
         </div>
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="description">Description</label>
-            <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="description" name="description" placeholder="wth is happening with that pipeline?!" />
+            <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="description" name="description" placeholder="wth is happening with that pipeline?!" value={description} onChange={(e) => { setDescription(e.target.value) }} />
         </div>
         <button className="px-8 py-1 bg-purple-900 text-white rounded-full cursor-pointer" type="submit">Add Note</button>
     </form>
