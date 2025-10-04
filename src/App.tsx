a93e4c0b9225d74dde37d2d9af19449202af1232
Felipe Almeida <felipeortalmeida@gmail.com>
2025-10-04 01:23:37 -0300

    feat: improve app styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import NoteForm from "./components/NoteForm"
 
-const App = () => <div className="flex flex-col gap-4 items-center justify-center w-full max-w-[640px] p-6 text-center bg-gray-100 rounded-lg shadow-lg">
-  <h1 className="text-2xl font-medium">📝 Notes</h1>
+const App = () => <div className="flex flex-col gap-4 items-stretch justify-center w-full max-w-[640px] p-6 bg-neutral-100 text-neutral-700 rounded-lg shadow-lg">
+  <h1 className="text-2xl font-medium text-center">📝 Notes</h1>
   <NoteForm />
 </div>
 
--- src/components/NoteForm/index.tsx
+++ src/components/NoteForm/index.tsx
@@ -1,8 +1,20 @@
-const NoteForm = () => <form>
-    <div><label htmlFor="title">Title</label><input type="text" id="title" name="title" /></div>
-    <div><label htmlFor="priority">Priority</label><input type="text" id="priority" name="priority" /></div>
-    <div><label htmlFor="category">Category</label><input type="text" id="category" name="category" /></div>
-    <div><label htmlFor="description">Description</label><input type="text" id="description" name="description" /></div>
+const NoteForm = () => <form className="flex flex-col items-stretch justify-center gap-3">
+    <div className="flex flex-col gap-1 items-stretch justify-between">
+        <label className="font-medium" htmlFor="title">Title</label>
+        <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="title" name="title" placeholder="fix github actions yml" />
+    </div>
+    <div className="flex flex-col gap-1 items-stretch justify-between">
+        <label className="font-medium" htmlFor="priority">Priority</label>
+        <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="priority" name="priority" placeholder="high" />
+    </div>
+    <div className="flex flex-col gap-1 items-stretch justify-between">
+        <label className="font-medium" htmlFor="category">Category</label>
+        <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="category" name="category" placeholder="work" />
+    </div>
+    <div className="flex flex-col gap-1 items-stretch justify-between">
+        <label className="font-medium" htmlFor="description">Description</label>
+        <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="description" name="description" placeholder="wth is happening with that pipeline?!" />
+    </div>
 </form>
 
 export default NoteForm
