c60d43d7b8dedb6042bdb9b4d79c3e09cd9ca0de
Felipe Almeida <felipeortalmeida@gmail.com>
2025-10-15 00:01:54 -0300

    feat: improve form UI

--- src/components/NoteForm/index.tsx
+++ src/components/NoteForm/index.tsx
@@ -8,33 +8,35 @@ const NoteForm = () => {
 
     console.log(title, priority, category, description)
 
-    return <form className="flex flex-col items-stretch justify-center gap-3">
+    return <form className="flex flex-col gap-3 items-stretch justify-center">
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="title">Title</label>
-            <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="title" name="title" placeholder="fix github actions yml" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+            <input className="h-8 px-2 border border-neutral-300 rounded-full" type="text" id="title" name="title" placeholder="fix github actions yml" value={title} onChange={(e) => { setTitle(e.target.value) }} />
         </div>
-        <div className="flex flex-col gap-1 items-stretch justify-between">
-            <label className="font-medium" htmlFor="priority">Priority</label>
-            <select className="px-2 py-1 border border-neutral-300 rounded-xl" name="priority" id="priority" value={priority} onChange={(e) => { setPriority(e.target.value) }}>
-                <option value="">select a priority</option>
-                <option>high</option>
-                <option>medium</option>
-                <option>low</option>
-            </select>
-        </div>
-        <div className="flex flex-col gap-1 items-stretch justify-between">
-            <label className="font-medium" htmlFor="category">Category</label>
-            <select className="px-2 py-1 border border-neutral-300 rounded-xl" name="category" id="category" value={category} onChange={(e) => { setCategory(e.target.value) }}>
-                <option value="">select a category</option>
-                <option>work</option>
-                <option>personal</option>
-            </select>
+        <div className="flex flex-col gap-2 sm:flex-row sm:items-stretch sm:justify-between">
+            <div className="flex flex-col gap-1 items-stretch justify-between sm:basis-4/12">
+                <label className="font-medium" htmlFor="priority">Priority</label>
+                <select className="h-8 px-2 border border-neutral-300 rounded-full" name="priority" id="priority" value={priority} onChange={(e) => { setPriority(e.target.value) }}>
+                    <option value="">select a priority</option>
+                    <option>high</option>
+                    <option>medium</option>
+                    <option>low</option>
+                </select>
+            </div>
+            <div className="flex flex-col gap-1 items-stretch justify-between sm:basis-8/12">
+                <label className="font-medium" htmlFor="category">Category</label>
+                <select className="h-8 px-2 border border-neutral-300 rounded-full" name="category" id="category" value={category} onChange={(e) => { setCategory(e.target.value) }}>
+                    <option value="">select a category</option>
+                    <option>work</option>
+                    <option>personal</option>
+                </select>
+            </div>
         </div>
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="description">Description</label>
-            <input className="px-2 py-1 border border-neutral-300 rounded-xl" type="text" id="description" name="description" placeholder="wth is happening with that pipeline?!" value={description} onChange={(e) => { setDescription(e.target.value) }} />
+            <input className="h-8 px-2 border border-neutral-300 rounded-full" type="text" id="description" name="description" placeholder="wth is happening with that pipeline?!" value={description} onChange={(e) => { setDescription(e.target.value) }} />
         </div>
-        <button className="px-8 py-1 bg-purple-900 text-white rounded-full cursor-pointer" type="submit">Add Note</button>
+        <button className="h-8 mt-1 px-8 bg-purple-900 text-white rounded-full cursor-pointer" type="submit">Add Note</button>
     </form>
 }
 
