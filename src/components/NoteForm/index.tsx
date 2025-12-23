ba3e3bbd47500ea0908b580637a076345cb84044
Felipe Almeida <felipeortalmeida@gmail.com>
2025-12-22 23:32:02 -0300

    refactor: use single state variable to handle form state

--- src/components/NoteForm/index.tsx
+++ src/components/NoteForm/index.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react"
+import { useState, type ChangeEventHandler } from "react"
 
 const NoteForm = () => {
-    const [title, setTitle] = useState("")
-    const [priority, setPriority] = useState("")
-    const [category, setCategory] = useState("")
-    const [description, setDescription] = useState("")
+    const [form, setForm] = useState({
+        title: "",
+        priority: "",
+        category: "",
+        description: "",
+    })
 
-    console.log(title, priority, category, description)
+    const handleChange: ChangeEventHandler<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement> = ({ target: { name, value } }) => {
+        setForm((prevForm) => ({ ...prevForm, [name]: value }))
+    }
 
     return <form className="flex flex-col gap-3 items-stretch justify-center">
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="title">Title</label>
-            <input className="h-8 px-3 border border-neutral-300 rounded-full" type="text" id="title" name="title" placeholder="fix github actions yml" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+            <input className="h-8 px-3 border border-neutral-300 rounded-full" type="text" id="title" name="title" placeholder="fix github actions yml" value={form.title} onChange={handleChange} />
         </div>
         <div className="flex flex-col gap-2 sm:flex-row sm:items-stretch sm:justify-between">
             <div className="flex flex-col gap-1 items-stretch justify-between sm:basis-4/12">
                 <label className="font-medium" htmlFor="priority">Priority</label>
-                <select className="h-8 px-3 border border-neutral-300 rounded-full" name="priority" id="priority" value={priority} onChange={(e) => { setPriority(e.target.value) }}>
+                <select className="h-8 px-3 border border-neutral-300 rounded-full" name="priority" id="priority" value={form.priority} onChange={handleChange}>
                     <option value="">select a priority</option>
                     <option>high</option>
                     <option>medium</option>
@@ -25,7 +29,7 @@ const NoteForm = () => {
             </div>
             <div className="flex flex-col gap-1 items-stretch justify-between sm:basis-8/12">
                 <label className="font-medium" htmlFor="category">Category</label>
-                <select className="h-8 px-3 border border-neutral-300 rounded-full" name="category" id="category" value={category} onChange={(e) => { setCategory(e.target.value) }}>
+                <select className="h-8 px-3 border border-neutral-300 rounded-full" name="category" id="category" value={form.category} onChange={handleChange}>
                     <option value="">select a category</option>
                     <option>work</option>
                     <option>personal</option>
@@ -34,7 +38,7 @@ const NoteForm = () => {
         </div>
         <div className="flex flex-col gap-1 items-stretch justify-between">
             <label className="font-medium" htmlFor="description">Description</label>
-            <textarea className="h-40 px-3 py-2 border border-neutral-300 rounded-2xl resize-none" id="description" name="description" placeholder="wth is happening with that pipeline?!" value={description} onChange={(e) => { setDescription(e.target.value) }}></textarea>
+            <textarea className="h-40 px-3 py-2 border border-neutral-300 rounded-2xl resize-none" id="description" name="description" placeholder="wth is happening with that pipeline?!" value={form.description} onChange={handleChange}></textarea>
         </div>
         <button className="h-8 mt-1 px-8 bg-purple-900 text-white rounded-full cursor-pointer" type="submit">Add Note</button>
     </form>
