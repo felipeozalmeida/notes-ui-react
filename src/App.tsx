04ef8634d08544965597d2c02de9ffd39426ce2f
Felipe Almeida <felipeortalmeida@gmail.com>
2025-12-27 01:02:58 -0300

    feat: add react-hot-toast for notifications and update NoteList to show toast on ID copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Toaster } from "react-hot-toast";
 import type { Note } from "./types"
 import NoteForm from "./components/NoteForm"
 import NoteList from "./components/NoteList";
@@ -7,11 +8,14 @@ const App = () => {
   const [notes, setNotes] = useState<Note[]>([]);
 
   return (
-    <div className="flex flex-col gap-4 items-stretch justify-center w-full max-w-[640px] p-6 bg-neutral-100 text-neutral-700 rounded-lg shadow-lg">
-      <h1 className="text-2xl font-medium text-center">📝 Notes</h1>
-      <NoteForm onCreate={(note) => setNotes((prevNotes) => [...prevNotes, note])} />
-      <NoteList notes={notes} />
-    </div>
+    <>
+      <div className="flex flex-col gap-4 items-stretch justify-center w-full max-w-[640px] p-6 bg-neutral-100 text-neutral-700 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-medium text-center">📝 Notes</h1>
+        <NoteForm onCreate={(note) => setNotes((prevNotes) => [...prevNotes, note])} />
+        <NoteList notes={notes} />
+      </div>
+      <Toaster />
+    </>
   );
 }
 
